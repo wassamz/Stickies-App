ca0c40913f43729b56e4b2403d71c31b69d27b28
Wassam <wassam@wassam.com>
2025-01-27 18:01:29 -0500

    Add OTP input component and reset password form with validation

---
+++ src/components/OTP.js
@@ -0,0 +1,158 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { Box, TextField } from '@mui/material';
+
+function Otp({ separator, length, value, onChange }) {
+  const inputRefs = React.useRef(new Array(length).fill(null));
+
+  const focusInput = (targetIndex) => {
+    const targetInput = inputRefs.current[targetIndex];
+    targetInput.focus();
+  };
+
+  const selectInput = (targetIndex) => {
+    const targetInput = inputRefs.current[targetIndex];
+    targetInput.select();
+  };
+
+  const handleKeyDown = (event, currentIndex) => {
+    switch (event.key) {
+      case 'ArrowUp':
+      case 'ArrowDown':
+      case ' ':
+        event.preventDefault();
+        break;
+      case 'ArrowLeft':
+        event.preventDefault();
+        if (currentIndex > 0) {
+          focusInput(currentIndex - 1);
+          selectInput(currentIndex - 1);
+        }
+        break;
+      case 'ArrowRight':
+        event.preventDefault();
+        if (currentIndex < length - 1) {
+          focusInput(currentIndex + 1);
+          selectInput(currentIndex + 1);
+        }
+        break;
+      case 'Delete':
+        event.preventDefault();
+        onChange((prevOtp) => {
+          const otp =
+            prevOtp.slice(0, currentIndex) + prevOtp.slice(currentIndex + 1);
+          return otp;
+        });
+        break;
+      case 'Backspace':
+        event.preventDefault();
+        if (currentIndex > 0) {
+          focusInput(currentIndex - 1);
+          selectInput(currentIndex - 1);
+        }
+        onChange((prevOtp) => {
+          const otp =
+            prevOtp.slice(0, currentIndex) + prevOtp.slice(currentIndex + 1);
+          return otp;
+        });
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleChange = (event, currentIndex) => {
+    const currentValue = event.target.value.replace(/\D/g, ''); // Only allow digits
+    let indexToEnter = 0;
+
+    while (indexToEnter <= currentIndex) {
+      if (inputRefs.current[indexToEnter].value && indexToEnter < currentIndex) {
+        indexToEnter += 1;
+      } else {
+        break;
+      }
+    }
+    onChange((prev) => {
+      const otpArray = prev.split('');
+      const lastValue = currentValue[currentValue.length - 1];
+      otpArray[indexToEnter] = lastValue;
+      return otpArray.join('');
+    });
+    if (currentValue !== '') {
+      if (currentIndex < length - 1) {
+        focusInput(currentIndex + 1);
+      }
+    }
+  };
+
+  const handleClick = (event, currentIndex) => {
+    selectInput(currentIndex);
+  };
+
+  const handlePaste = (event, currentIndex) => {
+    event.preventDefault();
+    const clipboardData = event.clipboardData;
+
+    // Check if there is text data in the clipboard
+    if (clipboardData.types.includes('text/plain')) {
+      let pastedText = clipboardData.getData('text/plain');
+      pastedText = pastedText.substring(0, length).trim();
+      let indexToEnter = 0;
+
+      while (indexToEnter <= currentIndex) {
+        if (inputRefs.current[indexToEnter].value && indexToEnter < currentIndex) {
+          indexToEnter += 1;
+        } else {
+          break;
+        }
+      }
+
+      const otpArray = value.split('');
+
+      for (let i = indexToEnter; i < length; i += 1) {
+        const lastValue = pastedText[i - indexToEnter] ?? ' ';
+        otpArray[i] = lastValue;
+      }
+
+      onChange(otpArray.join(''));
+    }
+  };
+
+  return (
+    <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+      {new Array(length).fill(null).map((_, index) => (
+        <React.Fragment key={`otp-${index}`}>
+          <TextField
+            key={index}
+            data-testid={`otp-input-${index}`}
+            variant="outlined"
+            slotProps={{
+              input: {
+                style: { textAlign: 'center' },
+                maxLength: 1,
+              },
+            }}
+            inputRef={(ele) => {
+              inputRefs.current[index] = ele;
+            }}
+            onKeyDown={(event) => handleKeyDown(event, index)}
+            onChange={(event) => handleChange(event, index)}
+            onClick={(event) => handleClick(event, index)}
+            onPaste={(event) => handlePaste(event, index)}
+            value={value[index] ?? ''}
+          />
+          {index === length - 1 ? null : separator}
+        </React.Fragment>
+      ))}
+    </Box>
+  );
+}
+
+Otp.propTypes = {
+  length: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  separator: PropTypes.node,
+  value: PropTypes.string.isRequired,
+};
+
+export default Otp;
---
+++ src/components/ResetForm.js
@@ -0,0 +1,202 @@
+import { Box } from "@mui/material";
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { forgotPassword, resetPassword } from "../services/Api";
+import { authFormType, showForm, statusCode } from "../util/auth";
+import {
+  errorReason,
+  validEmail,
+  validPassword,
+} from "../util/inputValidation";
+import Otp from "./OTP";
+import "./styles/Form.css";
+
+// Prop Types validation
+ResetForm.propTypes = {
+  submit: PropTypes.func.isRequired,
+  toggleForm: PropTypes.func.isRequired,
+  errorMessage: PropTypes.func.isRequired,
+  infoMessage: PropTypes.func.isRequired,
+};
+
+function ResetForm({ submit, toggleForm, errorMessage, infoMessage }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [otp, setOtp] = useState("");
+  const [otpSendCount, setOtpSendCount] = useState(0);
+  const [timer, setTimer] = useState(600); // 10 minutes in seconds
+
+  useEffect(() => {
+    if (otpSendCount > 0 && timer > 0) {
+      const interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+    if (timer === 0) {
+      errorMessage(errorReason.OTP_EXPIRED);
+      infoMessage(null);
+    }
+  }, [errorMessage, infoMessage, otpSendCount, timer]);
+
+  const handleShowForm = showForm(setFormData, toggleForm);
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleOTPChange = (newOtp) => {
+    setOtp(newOtp);
+  };
+
+  const sendOTP = async () => {
+    errorMessage(null);
+    infoMessage(null);
+
+    if (!validEmail(formData.email)) {
+      errorMessage(errorReason.EMAIL_INVALID);
+      return;
+    }
+    //call api to send OTP if email is valid
+    const result = await forgotPassword(formData.email);
+    infoMessage(
+      "If your email is assigned to an account, a 4 digit One Time Password has been " +
+        (otpSendCount > 0 ? "re" : "") +
+        "sent to your email address."
+    );
+    if (result.status === statusCode.SUCCESS) {
+      setOtpSendCount(otpSendCount + 1);
+    }
+  };
+
+  const handleResetPassword = async () => {
+    infoMessage(null);
+    errorMessage(null);
+
+    if (!validEmail(formData.email)) {
+      errorMessage(errorReason.EMAIL_INVALID);
+      return;
+    }
+    if (!validPassword(formData.password)) {
+      errorMessage(errorReason.PASSWORD_INVALID);
+      return;
+    }
+    if (!otp || otp.length !== 4) {
+      errorMessage(errorReason.OTP_INVALID);
+    }
+
+    //send API call to reset password using otp, email, and new password
+    const result = await resetPassword(formData.email, formData.password, otp);
+
+    if (result.status === statusCode.SUCCESS) submit(formData);
+    else errorMessage(errorReason.OTP_INVALID);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="auth-container">
+        <form onSubmit={handleSubmit} className="auth-form">
+          <h2 className="auth-title">My Stickies Password Reset</h2>
+          <div className="form-group">
+            <p>Reset your password using your email.</p>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">E-mail</label>
+            <input
+              data-testid="reset-email"
+              type="text"
+              id="email"
+              name="email"
+              required
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          {otpSendCount > 0 && (
+            <div className="form-group">
+              <label htmlFor="otp">Enter 4 Digit Code</label>
+              <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                <Otp
+                  separator={<span>-</span>}
+                  value={otp}
+                  onChange={handleOTPChange}
+                  length={4}
+                />
+                <div className="otp-info-container">
+                  <span data-testid="otp-value">Entered value: {otp}</span>
+                  <span data-testid="timer-value">
+                    Time remaining: {formatTime(timer)}
+                  </span>
+                </div>
+              </Box>
+            </div>
+          )}
+          {otpSendCount > 0 && (
+            <div className="form-group">
+              <label htmlFor="newPassword">New Password</label>
+              <input
+                data-testid="reset-password"
+                type="password"
+                id="password"
+                name="password"
+                required
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
+          )}
+
+          <div className="button-container">
+            <button
+              data-testid="reset-login-user-button"
+              type="button"
+              className="alter-btn"
+              onClick={() => handleShowForm(authFormType.LOGIN)}
+            >
+              Back
+            </button>
+            <button
+              data-testid="reset-send-otp-button"
+              type="submit"
+              className="submit-btn"
+              onClick={sendOTP}
+              disabled={!validEmail(formData.email) || timer <= 0}
+            >
+              {otpSendCount > 0 ? "Resend Code" : "Send Code"}
+            </button>
+            {otpSendCount > 0 && (
+              <button
+                data-testid="reset-submit-otp-button"
+                type="submit"
+                className="submit-btn"
+                disabled={otp.length !== 4 || timer <= 0}
+                onClick={handleResetPassword}
+              >
+                Reset Password
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ResetForm;
