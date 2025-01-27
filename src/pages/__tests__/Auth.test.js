b0925ad4b1e7f86257c69630731d9bfc9b370d2d
Wassam <wassam@wassam.com>
2025-01-27 18:03:29 -0500

    Refactor Auth component to support toggling between Login, Sign Up, and Reset forms; enhance error handling and user feedback

--- src/pages/__tests__/Auth.test.js
+++ src/pages/__tests__/Auth.test.js
@@ -1,8 +1,9 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { useNavigate } from "react-router-dom";
+import { UserProvider, useUserProfile } from "../../context/UserContext";
 import { login, signUp } from "../../services/Api";
 import Auth from "../Auth";
-import { UserProvider, useUserProfile } from "../../context/UserContext";
+import { errorReason } from "../../util/inputValidation";
 
 // Mock the login and signUp services
 jest.mock("../../services/Api", () => ({
@@ -48,55 +49,59 @@ describe("Auth Component", () => {
   });
 
   it("renders the SignUpForm when toggled", () => {
-    const createAccountButton = screen.getByTestId("sign-up-user-button");
-    fireEvent.click(createAccountButton);
+    const signUpFormButton = screen.getByTestId("sign-up-user-form-button");
+    fireEvent.click(signUpFormButton);
     expect(screen.getByText(/My Stickies Sign Up/i)).toBeInTheDocument();
   });
 
   it("calls login service on form submit and shows error if login fails", async () => {
     login.mockResolvedValueOnce({
       status: "ERROR",
-      message: "Invalid credentials",
+      message: errorReason.EMAIL_INVALID,
     });
 
     const submitButton = screen.getByTestId("login-user-button");
     fireEvent.click(submitButton);
 
     await waitFor(() =>
-      expect(screen.getByText(/invalid credentials/i)).toBeInTheDocument()
-    );
-  });
-
-  it("calls signUp service on form submit and shows success message on successful signup", async () => {
-    signUp.mockResolvedValueOnce({
-      status: "SUCCESS",
-      message: "Account created successfully",
-    });
-
-    const createAccountButton = screen.getByTestId("sign-up-user-button");
-    fireEvent.click(createAccountButton);
-
-    const submitButton = screen.getByText(/create account/i);
-    fireEvent.click(submitButton);
-
-    await waitFor(() =>
       expect(
-        screen.getByText(/account created successfully/i)
+        screen.getByText(errorReason.EMAIL_INVALID)
       ).toBeInTheDocument()
     );
   });
 
-  it("navigates to /notes on successful login/signup", async () => {
-    login.mockResolvedValueOnce({
+  it("navigates to /notes on successful Sign Up", async () => {
+    signUp.mockResolvedValueOnce({
       status: "SUCCESS",
-      message: "Logged in successfully",
+      message: "User Created",
     });
+
+    // Open Sign Up Form
+    const signUpFormButton = screen.getByTestId("sign-up-user-form-button");
+    fireEvent.click(signUpFormButton);
+
+    // Select inputs and submit button
+    const nameInput = screen.getByTestId("sign-up-name");
+    const emailInput = screen.getByTestId("sign-up-email");
+    const passwordInput = screen.getByTestId("sign-up-password");
+    const signUpButton = screen.getByTestId("sign-up-user-button");
+    const mockSubmitData = {
+      name: "John Doe",
+      email: "john@example.com",
+      password: "Password123$",
+    };
+
+    // Simulate user input
+    fireEvent.change(nameInput, { target: { value: mockSubmitData.name } });
+    fireEvent.change(emailInput, { target: { value: mockSubmitData.email } });
+    fireEvent.change(passwordInput, {
+      target: { value: mockSubmitData.password },
+    });
+
+    fireEvent.click(signUpButton);
 
     // Ensure useNavigate is mocked properly inside beforeEach
     const navigate = useNavigate();
-
-    const loginButton = screen.getByTestId("login-user-button");
-    fireEvent.click(loginButton);
 
     await waitFor(() => {
       expect(navigate).toHaveBeenCalledWith(
@@ -106,17 +111,45 @@ describe("Auth Component", () => {
     });
   });
 
-  it("displays the error message if login/signup fails", async () => {
+  it("displays the error message if login if email is invalid", async () => {
     login.mockResolvedValueOnce({
       status: "ERROR",
       message: "Unable to login",
     });
-
+    const emailInput = screen.getByTestId("login-email");
+    const passwordInput = screen.getByTestId("login-password");
     const loginButton = screen.getByTestId("login-user-button");
+
+    // Simulate user input
+    fireEvent.change(emailInput, { target: { value: "example.com" } });
+    fireEvent.change(passwordInput, { target: { value: "Password123$" } });
+
     fireEvent.click(loginButton);
 
     await waitFor(() =>
-      expect(screen.getByText(/Unable to login/i)).toBeInTheDocument()
+      expect(
+        screen.getByText(errorReason.EMAIL_INVALID)
+      ).toBeInTheDocument()
+    );
+  });
+  it("displays the error message if login password is invalid", async () => {
+    login.mockResolvedValueOnce({
+      status: "ERROR",
+      message: "Unable to login",
+    });
+    const emailInput = screen.getByTestId("login-email");
+    const passwordInput = screen.getByTestId("login-password");
+    const loginButton = screen.getByTestId("login-user-button");
+
+    // Simulate user input
+    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.click(loginButton);
+
+    await waitFor(() =>
+      expect(
+        screen.getByText(errorReason.PASSWORD_INVALID)
+      ).toBeInTheDocument()
     );
   });
 
@@ -125,8 +158,14 @@ describe("Auth Component", () => {
       status: "SUCCESS",
       message: "Login Successful",
     });
-
+    const emailInput = screen.getByTestId("login-email");
+    const passwordInput = screen.getByTestId("login-password");
     const loginButton = screen.getByTestId("login-user-button");
+
+    // Simulate user input
+    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+    fireEvent.change(passwordInput, { target: { value: "Password123$" } });
+
     fireEvent.click(loginButton);
 
     await waitFor(() =>
